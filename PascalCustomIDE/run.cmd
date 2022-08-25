5b56a1f4a7a658145b3084b857ba3ab1e6d544a0
kynguyen321 <72696885+kynguyen321@users.noreply.github.com>
2022-08-25 22:10:25 +0700

    Update run.cmd

--- PascalCustomIDE/run.cmd
+++ PascalCustomIDE/run.cmd
@@ -3,6 +3,7 @@
 fpc -Aas ".\PascalFile\Untitled-1.txt"
 cls
 .\PascalFile\Untitled-1.exe
+echo.
 echo =====================================================
 echo = Nhấn một phím bất kì để chạy lại chương trình ... =
 echo = Liên hệ tôi tại admin@40l8kw.onmicrosoft.com      =
