986c4600b44bbd9c4dbbe3c5850fc0a5851373d9
kynguyen321 <72696885+kynguyen321@users.noreply.github.com>
2022-08-25 15:20:28 +0700

    Add files via upload

---
+++ PascalCustomIDE/run.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+:end
+fpc -Aas ".\PascalFile\Untitled-1.pas"
+cls
+.\PascalFile\Untitled-1.exe
+echo =====================================================
+echo = Nhấn một phím bất kì để chạy lại chương trình ... =
+echo = Liên hệ tôi tại admin@40l8kw.onmicrosoft.com      =
+echo =====================================================
+pause >nul
+goto end
