336a69e63d4a12b3ea4848609f4062ce0e120c62
kynguyen321 <72696885+kynguyen321@users.noreply.github.com>
2022-08-26 09:59:02 +0700

    Update run.cmd

--- PascalCustomIDE/run.cmd
+++ PascalCustomIDE/run.cmd
@@ -6,7 +6,11 @@ del Untitled-1.o
 cd ..\
 fpc -Aas ".\PascalFile\Untitled-1.txt"
 cls
+IF EXIST .\PascalFile\Untitled-1.exe (
 .\PascalFile\Untitled-1.exe
+) ELSE (
+fpc -Aas ".\PascalFile\Untitled-1.txt"
+)
 echo.
 echo =====================================================
 echo = Nhấn một phím bất kì để chạy lại chương trình ... =
